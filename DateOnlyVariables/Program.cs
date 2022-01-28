db9dd3b65093f2be70bd5e5b2b8c591d81b3d58a
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-28 09:34:12 -0600

    completed exercise

---
+++ DateOnlyVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+//.net 6 and beyond
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("6/11/1998");
+Console.WriteLine(birthday.ToString("MMMM dd,yyyy"));
+
+Console.WriteLine($"Today full format:{today}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full formatt: {birthday}");
+
+Console.ReadLine();
